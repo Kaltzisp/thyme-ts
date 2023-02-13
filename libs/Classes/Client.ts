091e3fb3fccda4f7ac5013a0b4b43e2347d597d6
Peter Kaltzis <peterkaltzis@outlook.com>
2023-02-14 04:02:25 +1100

    added command registration

--- libs/Classes/Client.ts
+++ libs/Classes/Client.ts
@@ -1,16 +1,37 @@
+import { Client, REST, Routes } from "discord.js";
+import { type Command, importAll } from "../core/registerCommands.js";
 import type { BotConfig } from "../config.js";
-import { Client } from "discord.js";
 
 class ThymeClient extends Client {
     public config: BotConfig;
+    public cmds: unknown;
 
     public constructor(config: BotConfig) {
         super({ intents: config.intents });
         this.config = config;
     }
 
-    public initialize(): void {
+    public async initialize(): Promise<void> {
         this.logStart();
+        await this.registerCommands();
+    }
+
+    private async registerCommands(): Promise<void> {
+        // Registering commands.
+        const libs = (await Promise.all(importAll("./app/commands"))).map(lib => lib.default);
+        const rest = new REST({ version: "10" }).setToken(process.env.BOT_TOKEN!);
+        await rest.put(Routes.applicationCommands(this.user!.id), { body: libs });
+        // Restructuring to object.
+        const modules: Record<string, Command> = libs.reduce((accumulator: Record<string, Command>, current) => {
+            accumulator[current.name] = current;
+            return accumulator;
+        }, {});
+        // Assigning command exes.
+        this.on("interactionCreate", (interaction) => {
+            if (interaction.isChatInputCommand()) {
+                modules[interaction.commandName].exe(interaction);
+            }
+        });
     }
 
     private logStart(): void {
@@ -26,7 +47,7 @@ class ThymeClient extends Client {
             hour12: false,
             timeZone: "Australia/Melbourne"
         }).split(", ");
-        time = `BOOTED CLIENT: ${this.user?.username} @ ${time[2]} - ${time[0]} ${time[1]}`;
+        time = `BOOTED CLIENT: ${this.user!.username} @ ${time[2]} - ${time[0]} ${time[1]}`;
         const separator = new Array(time.length + 1).join("=");
         console.log(`\n\n${separator}\n${time}\n${separator}\n\n`);
     }
