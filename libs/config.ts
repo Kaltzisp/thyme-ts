ef94a24480092fa5eb92da13bddef9a6a078a431
Peter Kaltzis <peterkaltzis@outlook.com>
2023-02-14 04:01:51 +1100

    assert process.env types

--- libs/config.ts
+++ libs/config.ts
@@ -4,16 +4,16 @@ import dotenv from "dotenv";
 dotenv.config();
 
 interface BotConfig {
-    token: string | undefined;
+    token: string;
     admins: string[] | undefined;
     evalChannels: string[] | undefined;
     intents: number[];
 }
 
 const config: BotConfig = {
-    token: process.env.BOT_TOKEN,
-    admins: process.env.ADMINS?.split(","),
-    evalChannels: process.env.EVAL_CHANNELS?.split(","),
+    token: process.env.BOT_TOKEN!,
+    admins: process.env.ADMINS!.split(","),
+    evalChannels: process.env.EVAL_CHANNELS!.split(","),
     intents: [GatewayIntentBits.GuildInvites]
 };
 
