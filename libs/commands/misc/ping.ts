0a842b0d51ee7dd0b5234a30468a52a766a40afd
Peter Kaltzis <peterkaltzis@outlook.com>
2023-02-14 13:24:00 +1100

    modified dummy command

--- libs/commands/misc/ping.ts
+++ libs/commands/misc/ping.ts
@@ -1,13 +1,15 @@
-import type { Message } from "discord.js";
+import type { Interaction } from "discord.js";
 
 export default {
-    alias: "ping",
-    info: "Sends the user and client ping.",
+    name: "ping",
+    alias: ["ping"],
+    description: "Sends the user and client ping.",
     args: [],
-    async exe(msg: Message): Promise<void> {
-        const m = await msg.channel.send("Ping!");
-        const myPing = Math.round(m.createdTimestamp - msg.createdTimestamp);
-        const clientPing = Math.round(msg.client.ws.ping);
-        m.edit(`>>> Pong! Latency is ${myPing}ms. API Latency is ${clientPing}ms.`).catch(e => console.log(e));
+    exe(interaction: Interaction): void {
+        console.log(interaction);
+        // const m = await msg.channel.send("Ping!");
+        // const myPing = Math.round(m.createdTimestamp - msg.createdTimestamp);
+        // const clientPing = Math.round(msg.client.ws.ping);
+        // m.edit(`>>> Pong! Latency is ${myPing}ms. API Latency is ${clientPing}ms.`).catch(e => console.log(e));
     }
 };
