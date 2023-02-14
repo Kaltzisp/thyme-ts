741832d4043b6c494bee51f91fd4b6f4ae7f1b85
Peter Kaltzis <peterkaltzis@outlook.com>
2023-02-14 13:39:00 +1100

    updated ping command

--- libs/commands/misc/ping.ts
+++ libs/commands/misc/ping.ts
@@ -1,15 +1,14 @@
-import type { Interaction } from "discord.js";
+import type { ChatInputCommandInteraction, Message } from "discord.js";
 
 export default {
     name: "ping",
     alias: ["ping"],
     description: "Sends the user and client ping.",
     args: [],
-    exe(interaction: Interaction): void {
-        console.log(interaction);
-        // const m = await msg.channel.send("Ping!");
-        // const myPing = Math.round(m.createdTimestamp - msg.createdTimestamp);
-        // const clientPing = Math.round(msg.client.ws.ping);
-        // m.edit(`>>> Pong! Latency is ${myPing}ms. API Latency is ${clientPing}ms.`).catch(e => console.log(e));
+    async exe(interaction: ChatInputCommandInteraction): Promise<void> {
+        const msg = await interaction.reply({ content: "Ping!", fetchReply: true }).catch(err => console.log(err)) as Message;
+        const myPing = Math.round(msg.createdTimestamp - interaction.createdTimestamp);
+        const clientPing = interaction.client.ws.ping;
+        msg.edit(`>>> Pong! Latency is ${myPing}ms. API Latency is ${clientPing}ms.`).catch(e => console.log(e));
     }
 };
