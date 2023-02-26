06cfc671e0c3458d903ac74b80bce6062746d623
Peter Kaltzis <peterkaltzis@outlook.com>
2023-02-26 20:59:24 +1100

    added bot admins and name to config

--- libs/config.ts
+++ libs/config.ts
@@ -4,13 +4,17 @@ import dotenv from "dotenv";
 dotenv.config();
 
 interface BotConfig {
+    name: string;
     token: string;
-    evalChannels: string[] | undefined;
-    intents: number[];
+    admins: string[];
+    evalChannels: string[];
+    intents: GatewayIntentBits[];
 }
 
-const config: BotConfig = {
+const config = {
+    name: process.env.BOT_NAME!,
     token: process.env.BOT_TOKEN!,
+    admins: process.env.ADMINS!.split(","),
     evalChannels: process.env.EVAL_CHANNELS!.split(","),
     intents: [GatewayIntentBits.Guilds]
 };
