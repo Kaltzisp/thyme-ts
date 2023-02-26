5ebdc83186c3a173d2514419074b8b3a013d6569
Peter Kaltzis <peterkaltzis@outlook.com>
2023-02-26 21:33:34 +1100

    updated eval.ts

--- libs/commands/config/eval.ts
+++ libs/commands/config/eval.ts
@@ -5,21 +5,25 @@ export default {
     data: new SlashCommandBuilder()
         .setName("eval")
         .setDescription("Evaluates input on Node.js and returns the result.")
-        .addStringOption(option => option.setName("expression").setDescription("The expression to evaluate.")),
+        .addStringOption(option => option
+            .setName("expression")
+            .setDescription("The expression to evaluate.")
+            .setRequired(true)),
     execute(interaction: ChatInputCommandInteraction, config: BotConfig): void {
         if (config.admins.includes(interaction.member!.user.id)) {
+            const evalInput = interaction.options.getString("expression", true);
             let evalOutput = "";
             try {
                 // eslint-disable-next-line no-eval
-                evalOutput = eval(interaction.options.getString("expression", true)) as string;
+                evalOutput = eval(evalInput) as string;
             } catch (e) {
                 if (e instanceof Error) {
-                    evalOutput = e.message;
+                    evalOutput = `${e.message}\n${e.stack}`;
                 } else {
                     evalOutput = "Unknown error.";
                 }
             }
-            interaction.reply(evalOutput).catch(err => console.error(err));
+            interaction.reply(`\`\`\`js\n${evalInput}\n\n${evalOutput}\`\`\``).catch(err => console.error(err));
             console.log(evalOutput);
         }
     }
