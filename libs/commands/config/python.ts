187f3482c3aea8bcef0b9d9663a4c0db16189b7b
Peter Kaltzis <peterkaltzis@outlook.com>
2023-03-01 22:34:03 +1100

    added python command

---
+++ libs/commands/config/python.ts
@@ -0,0 +1,19 @@
+import { type ChatInputCommandInteraction, type Message, SlashCommandBuilder } from "discord.js";
+import { spawn } from "child_process";
+
+export default {
+    data: new SlashCommandBuilder()
+        .setName("python")
+        .setDescription("Runs a python file and displays the output.")
+        .addStringOption(option => option
+            .setName("path")
+            .setDescription("The path to the python file.")
+            .setRequired(true)),
+    async execute(interaction: ChatInputCommandInteraction): Promise<void> {
+        const msg = await interaction.reply({ content: "Running python...", fetchReply: true }).catch(err => console.log(err)) as Message;
+        const pythonProcess = spawn("python", [interaction.options.getString("path", true)]);
+        pythonProcess.stdout.on("data", (data) => {
+            msg.edit(`\`\`\`py\n${data}\n\`\`\``).catch(e => console.log(e));
+        });
+    }
+};
