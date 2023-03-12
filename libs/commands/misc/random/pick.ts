286e10f8e7f8d26220b2aedc576c38fcb06c3179
Peter Kaltzis <peterkaltzis@outlook.com>
2023-03-12 14:12:28 +1100

    moved pick.ts

--- libs/commands/misc/random/pick.ts
+++ libs/commands/misc/random/pick.ts
@@ -2,15 +2,15 @@ import { type ChatInputCommandInteraction, SlashCommandBuilder } from "discord.j
 
 export default {
     data: new SlashCommandBuilder()
-        .setName("pick")
-        .setDescription("Picks an option from a set of | separated choices")
+        .setName("choose")
+        .setDescription("Chooses an option from a set of | separated choices")
         .addStringOption(option => option
-            .setName("options")
+            .setName("choices")
             .setDescription("Options, separated by | .")
             .setRequired(true)),
     execute(interaction: ChatInputCommandInteraction): void {
-        const choices = interaction.options.getString("path", true).split("|");
-        const selection = choices[Math.floor(Math.random()*choices.length)];
+        const choices = interaction.options.getString("choices", true).split("|");
+        const selection = choices[Math.floor(Math.random() * choices.length)];
         interaction.reply(`I chose: \`${selection}\``).catch(e => console.log(e));
     }
 };
