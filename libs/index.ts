444881cb7813ebaac5ad86f112b9466f5462d971
Peter Kaltzis <peterkaltzis@outlook.com>
2023-09-18 00:06:23 +1000

    renamed libs directory to src

--- libs/index.ts
+++
@@ -1,9 +0,0 @@
-import { Events } from "discord.js";
-import { ThymeClient } from "./Classes/Client.js";
-import { config } from "./core/config.js";
-
-const client = new ThymeClient(config);
-client.login(config.token).catch(e => console.error(e));
-client.once(Events.ClientReady, () => {
-    client.initialize().catch(e => console.error(e));
-});
